ceb7e49ebdd596b94b9791c58ad1a16d8833b284
Mina Nagy Zaki <mnzaki@gmail.com>
2011-08-01 11:33:26 +0300

    lavfi: add asrc_abuffer - audio source buffer filter
    Originally based on code by Stefano Sabatini and S. N. Hemanth

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -38,6 +38,7 @@ void avfilter_register_all(void)
     REGISTER_FILTER (ANULL,       anull,       af);
     REGISTER_FILTER (ARESAMPLE,   aresample,   af);
 
+    REGISTER_FILTER (ABUFFER,     abuffer,     asrc);
     REGISTER_FILTER (ANULLSRC,    anullsrc,    asrc);
 
     REGISTER_FILTER (ABUFFERSINK, abuffersink, asink);
